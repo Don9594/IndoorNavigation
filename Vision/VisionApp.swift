22abc526fea3481c2afc13bcbb4e56b38b8bbb4b
Don Wijesinghe <menuwan.wijesinghe@gmail.com>
2022-04-23 18:15:22 -0500

    Initial Commit

---
+++ Vision/VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionApp.swift
+//  Vision
+//
+//  Created by Don wijesinghe on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
